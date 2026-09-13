553f8ae0119e19ecf1012b9b73e6d1a4d266be01
safderjeedar <38007370+safderjeedar@users.noreply.github.com>
2026-09-13 18:10:00 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<string,vector<string>> mp;
+        for(auto it:strs){
+            string str = it;
+            sort(str.begin(),str.end());
+            mp[str].push_back(it);
+
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
